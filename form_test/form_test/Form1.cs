84db7ed80efe884deba97ba58df4e90610058333
Faisal Habib <faisalhabib046@gmail.com>
2016-04-16 02:57:39 +0500

    new

---
+++ form_test/form_test/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_test
+{
+    public partial class Form1 : Form
+    {
+        int position_X = 10;
+        int position_Y = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ opencv_test/opencv_test/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace opencv_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, byte> My_Image = new Image<Bgr, byte>(op.FileName);
+                pictureBox1.Image = My_Image.ToBitmap();
+            }
+        }
+    }
+}
